5d589d6c9c53fa8fb287918522a35e2dad8b0187
kaustubha-kc <kaustubh07kc@gmail.com>
2023-05-21 14:07:33 +0530

    website commit

--- main_rfc.py
+++
@@ -1,9 +0,0 @@
-from prepro_rfc import Preprocessing
-from rfc import RandomForest
-def main():
-    dataset = Preprocessing.callingfromsheetthroughapi('1xTxPTnJM6kPVqOeIg6yNL7ktvbpLTgDMXhwZJByq0YY','rl/vrse-preprocessing-3a95b11cc6fd.json')
-    dataset=Preprocessing.commonpre(dataset)
-    accuracy = RandomForest.Rfc(dataset)
-    print(accuracy)
-    
-main()
